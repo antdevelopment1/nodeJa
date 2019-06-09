ca76112ce6b9385a9fdbdda57f7cc1091f20a8d7
April Copes <antdevelopment1@gmail.com>
2019-06-08 19:53:58 -0600

    Adds task collection

--- task/mongodb.js
+++ task/mongodb.js
@@ -48,5 +48,32 @@ MongoClient.connect(
     //     console.log(result.ops);
     //   }
     // );
+
+    // Create a get tasks collection
+    // Make three that have a description and completion value
+    // Integrate error handeling
+    // Check it with robo T
+    db.collection("tasks").insertMany(
+      [
+        {
+          description: "Take out trash",
+          completed: false
+        },
+        {
+          description: "Get ice cream",
+          completed: false
+        },
+        {
+          description: "Go to texas roadhouse",
+          completed: true
+        }
+      ],
+      (error, result) => {
+        if (error) {
+          console.log(error);
+        }
+        console.log(result.ops);
+      }
+    );
   }
 );
