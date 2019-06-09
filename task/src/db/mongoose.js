e23b1321ba899b0811d0ecc226d437cd6df61c4d
April Copes <antdevelopment1@gmail.com>
2019-06-09 00:26:02 -0600

    Adds tasks model, instance and save method

--- task/src/db/mongoose.js
+++ task/src/db/mongoose.js
@@ -15,16 +15,41 @@ const User = mongoose.model("User", {
   }
 });
 
-// This is a new instance of User
-const me = new User({
-  name: "April",
-  age: 28
+// Task class model
+const Task = mongoose.model("Task", {
+  description: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  }
 });
 
-me.save()
+// Creating a task instance
+const task = new Task({
+  description: "Finish current node js section",
+  completed: true
+});
+
+// Using a built in method of this instance from mongoose
+task
+  .save()
   .then(() => {
-    console.log(me);
+    console.log(task);
   })
   .catch(error => {
     console.log("error", error);
   });
+// This is a new instance of User
+// const me = new User({
+//   name: "April",
+//   age: 28
+// });
+
+// me.save()
+//   .then(() => {
+//     console.log(me);
+//   })
+//   .catch(error => {
+//     console.log("error", error);
+//   });
