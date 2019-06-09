adac4828c1ec778b2cd2425595643db424540a1e
April Copes <antdevelopment1@gmail.com>
2019-06-08 23:14:20 -0600

    retrieves data from database

--- task/mongodb.js
+++ task/mongodb.js
@@ -1,5 +1,4 @@
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
@@ -16,64 +15,28 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    // db.collection("users").insertOne(
-    //   {
-    //     name: "April",
-    //     age: 28
-    //   },
-    //   (error, result) => {
+    // Retrieve Function
+    // db.collection("users").findOne(
+    //   { _id: new ObjectID("5cfc68f707cbdb513c99c00a") },
+    //   (error, user) => {
     //     if (error) {
-    //       console.log("Unable to insert user");
+    //       console.log("Unable to find user");
     //     }
 
-    //     console.log(result.ops);
-    //   }
-    // );
-    // db.collection("users").insertMany(
-    //   [
-    //     {
-    //       name: "Jen",
-    //       age: 23
-    //     },
-    //     {
-    //       name: "August",
-    //       age: 22
-    //     }
-    //   ],
-    //   (error, result) => {
-    //     if (error) {
-    //       console.log("Couldn't insert users");
-    //     }
-
-    //     console.log(result.ops);
+    //     console.log(user);
     //   }
     // );
 
-    // Create a get tasks collection
-    // Make three that have a description and completion value
-    // Integrate error handeling
-    // Check it with robo T
-    db.collection("tasks").insertMany(
-      [
-        {
-          description: "Take out trash",
-          completed: false
-        },
-        {
-          description: "Get ice cream",
-          completed: false
-        },
-        {
-          description: "Go to texas roadhouse",
-          completed: true
-        }
-      ],
-      (error, result) => {
-        if (error) {
-          console.log(error);
-        }
-        console.log(result.ops);
-      }
-    );
+    db.collection("users")
+      .find({ age: 28 })
+      .toArray((error, users) => {
+        console.log(users);
+      });
+
+    db.collection("users")
+      .find({ age: 28 })
+      .count((error, count) => {
+        console.log(count);
+      });
   }
 );
