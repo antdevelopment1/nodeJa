3b7310224d2b8f7d4fa6f3af1bd7a8b93d2b90a5
April Copes <antdevelopment1@gmail.com>
2019-06-09 20:05:58 -0600

    sets up password and email documents

--- task/src/db/mongoose.js
+++ task/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
@@ -8,12 +9,56 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
 // Essentially a new class or model
 const User = mongoose.model("User", {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
   },
   age: {
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    }
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("Password cannot contain password");
+      }
+    }
   }
 });
+
+const me = new User({
+  name: "   Eric W  ",
+  email: "  Mike@calM.com",
+  password: "passstop"
+});
+
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch(error => {
+    console.log("error", error);
+  });
 
 // Task class model
 const Task = mongoose.model("Task", {
@@ -26,30 +71,18 @@ const Task = mongoose.model("Task", {
 });
 
 // Creating a task instance
-const task = new Task({
-  description: "Finish current node js section",
-  completed: true
-});
-
-// Using a built in method of this instance from mongoose
-task
-  .save()
-  .then(() => {
-    console.log(task);
-  })
-  .catch(error => {
-    console.log("error", error);
-  });
-// This is a new instance of User
-// const me = new User({
-//   name: "April",
-//   age: 28
+// const task = new Task({
+//   description: "Finish current node js section",
+//   completed: true
 // });
 
-// me.save()
+// Using a built in method of this instance from mongoose
+// task
+//   .save()
 //   .then(() => {
-//     console.log(me);
+//     console.log(task);
 //   })
 //   .catch(error => {
 //     console.log("error", error);
 //   });
+// This is a new instance of User
