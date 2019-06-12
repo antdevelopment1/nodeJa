5962eec3d17d32778c93d0dcdd8bab2e08eaf2c6
April Copes <antdevelopment1@gmail.com>
2019-06-11 19:46:16 -0600

    begins to set up log in credentials

--- task/src/models/user.js
+++ task/src/models/user.js
@@ -19,6 +19,7 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -41,6 +42,23 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    throw new Error("Unable to log in");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Unable to log in!");
+  }
+
+  return user;
+};
+
+// Hash the plain text password before saving
 userSchema.pre("save", async function(next) {
   const user = this;
 
--- task/src/routers/user.js
+++ task/src/routers/user.js
@@ -72,6 +72,18 @@ router.post("/users", async (req, res) => {
   //     });
 });
 
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    res.send(user);
+  } catch (error) {
+    res.status(400).send();
+  }
+});
+
 router.patch("/users/:id", async (req, res) => {
   const updates = Object.keys(req.body);
 
