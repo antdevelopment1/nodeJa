5d3dc4320bff6660daace1c7b9cba981a3264503
April Copes <antdevelopment1@gmail.com>
2019-06-11 20:47:53 -0600

    saves token to database

--- task/src/models/user.js
+++ task/src/models/user.js
@@ -40,7 +40,15 @@ const userSchema = new mongoose.Schema({
         throw new Error("Password cannot contain password");
       }
     }
-  }
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 });
 
 userSchema.statics.findByCredentials = async (email, password) => {
@@ -62,8 +70,13 @@ userSchema.statics.findByCredentials = async (email, password) => {
 userSchema.methods.generateAuthToken = async function() {
   const user = this;
   const token = jwt.sign({ _id: user._id.toString() }, "thisisnynewcourse");
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
   return token;
 };
+
 // Hash the plain text password before saving
 userSchema.pre("save", async function(next) {
   const user = this;
