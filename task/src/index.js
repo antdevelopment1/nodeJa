64b5690d2da86619a3ed9c3e90af13946a7dd16c
April Copes <antdevelopment1@gmail.com>
2019-06-12 19:03:41 -0600

    commented generic middleware function

--- task/src/index.js
+++ task/src/index.js
@@ -9,9 +9,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // How to send a general request that the site is down and will be back up based on any request that is sent
-app.use((req, res, next) => {
-  res.status(503).send("The site is currently down. Check back soon!");
-});
+// app.use((req, res, next) => {
+//   res.status(503).send("The site is currently down. Check back soon!");
+// });
 
 app.use(express.json());
 app.use(userRouter);
